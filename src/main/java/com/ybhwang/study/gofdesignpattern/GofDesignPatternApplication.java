2acaba7ee48c85b19b250f532180e716b6dc51bc
uelfqk <shhzg_zz@naver.com>
2023-10-30 23:04:30 +0900

    first commit

---
+++ src/main/java/com/ybhwang/study/gofdesignpattern/GofDesignPatternApplication.java
@@ -0,0 +1,13 @@
+package com.ybhwang.study.gofdesignpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GofDesignPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GofDesignPatternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ybhwang/study/gofdesignpattern/GofDesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ybhwang.study.gofdesignpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GofDesignPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
