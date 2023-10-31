bc604ba5b0b6654bc3b0fa7b28da7cee9f833535
uelfqk <shhzg_zz@naver.com>
2023-10-31 23:53:22 +0900

    팩토리 메소드 패턴 1부 - 패턴소개

---
+++ src/main/java/com/ybhwang/study/gofdesignpattern/factory_method/DefaultShipFactory.java
@@ -0,0 +1,43 @@
+package com.ybhwang.study.gofdesignpattern.factory_method;
+
+public class DefaultShipFactory {
+
+    public static Ship orderShip(String name, String email) {
+        if(name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("배 이름을 지어주세요.");
+        }
+
+        if(name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("배 이름을 지어주세요.");
+        }
+
+        prepareFor(name);
+
+        Ship ship = new Ship();
+        ship.setName(name);
+
+        if(name.equalsIgnoreCase("whiteShip")) {
+            ship.setLogo("\uD83D\uDEE5");
+        } else if(name.equalsIgnoreCase("blackShip")) {
+            ship.setLogo("닷");
+        }
+
+        if(name.equalsIgnoreCase("whiteShip")) {
+            ship.setColor("white");
+        } else if(name.equalsIgnoreCase("blackShip")) {
+            ship.setColor("black");
+        }
+
+        sendEmailTo(email, ship);
+
+        return ship;
+    }
+
+    private static void sendEmailTo(String email, Ship ship) {
+        System.out.println("to " + email + " " + ship.getName() + " 다 만들었습니다.");
+    }
+
+    private static void prepareFor(String name) {
+        System.out.println(name + " 만들 준비 중");
+    }
+}
---
+++ src/main/java/com/ybhwang/study/gofdesignpattern/factory_method/Ship.java
@@ -0,0 +1,14 @@
+package com.ybhwang.study.gofdesignpattern.factory_method;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Ship {
+
+    private String name;
+
+    private String color;
+
+    private String logo;
+}
